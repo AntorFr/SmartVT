4cb7e3949849712b2caea4d51d80a53693971256
Cyril C <sosapi91@gmail.com>
2012-11-16 13:48:47 +0100

    V0.5
    Gestion de vairable_watch pour les déclencheurs d'ouverture.
    Implémentation de la gestion des 2 tempos ouverture et fermeture.
    Json : ajout du set de la consigne pour donner par exple 19.5°C

--- L_SmartVT1.lua
+++ L_SmartVT1.lua
@@ -1,5 +1,33 @@
-        
-        function readVariableOrInit(lul_device, devicetype, name, defaultValue)
+        local SWP_SID =		"urn:upnp-org:serviceId:SwitchPower1"
+		local DOOR_SID =	"urn:micasaverde-com:serviceId:SecuritySensor1"
+
+		-- Fonction permettant de surveiller les variables presentes dans "inhibit Sensors"
+		function register_watch(data)
+            -- Gestion des capteurs
+            --if (Inibitors(data)) then
+             --   luup.variable_set (HVOS_SID, "ModeState", "Idle", lul_device)
+            --    luup.log("Inibitor detecte : arret du chauffage")
+            --    SetTargetTable("0",data.heaters,data.heaterMode)
+			--	luup.call_timer("updateStatus", 1, interval, "", tostring(lul_device))
+			--end		
+			for index = 1, #data.inhibitSensors, 1 do
+                local device = data.inhibitSensors[index]
+				local type_device = luup.devices[device].device_type -- On determine le SID en fonction de l'ID.
+				
+				if (0 > device) then
+                    device = 0 - device
+                end
+				
+				if type_device == DOOR_DT then -- En fonction du SID, on determine la variable a lire. A ameliorer peut etre.
+					luup.variable_watch("watch_callback", DOOR_SID, "Tripped", device)
+				elseif type_device == SWP_DT then
+					luup.variable_watch("watch_callback", SWP_SID, "Status", device)
+				end
+			end
+            return true
+		end
+		
+		function readVariableOrInit(lul_device, devicetype, name, defaultValue)
             local var = luup.variable_get(devicetype,name, lul_device)
             if (var == nil) then
                 var = defaultValue
